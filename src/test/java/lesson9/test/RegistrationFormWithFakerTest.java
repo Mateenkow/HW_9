330befbfb904b7323e4422b9ffd3b9ff0bd56111
mateenkow <glaymonow@gmail.com>
2023-10-05 17:19:31 +0300

    upd

--- src/test/java/lesson9/test/RegistrationFormWithFakerTest.java
+++ src/test/java/lesson9/test/RegistrationFormWithFakerTest.java
@@ -3,7 +3,7 @@ package lesson9.test;
 import driver.BaseTest;
 import lesson9.pages.RegistrationFormPage;
 import org.junit.jupiter.api.Test;
-import testData.TestData;
+import testData.Utils;
 
 /**
  * @author mateenkov
@@ -11,36 +11,36 @@ import testData.TestData;
 
 public class RegistrationFormWithFakerTest extends BaseTest {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-    TestData td = new TestData();
+    Utils u = new Utils();
 
     @Test
     void registrationFormTest() {
         registrationFormPage.openPage("Student Registration Form")
                 .removeBanner()
-                .setFirstName(td.NAME)
-                .setLastName(td.LAST_NAME)
-                .setGender(td.GENDER)
-                .setEmailUser(td.EMAIL)
-                .setNumberPhone(td.PHONE)
-                .setDateBirth(td.DAY, td.MONTH, td.YEAR)
-                .setSubject(td.SUBJECT)
-                .setHobbies(td.HOBBIES)
-                .uploadPicture(td.FILE)
-                .setAddress(td.ADDRESS)
-                .setState(td.STATE)
-                .setCity(td.CITY)
+                .setFirstName(u.getName())
+                .setLastName(u.getLastName())
+                .setGender(u.getGender())
+                .setEmailUser(u.getEmail())
+                .setNumberPhone(u.getPhone())
+                .setDateBirth(u.getDay(), u.getMonth(), u.getYear())
+                .setSubject(u.getSubjects())
+                .setHobbies(u.getHobbies())
+                .uploadPicture(u.getFiles())
+                .setAddress(u.getAddress())
+                .setState("NCR") // Тут не разобрался
+                .setCity("Delhi") // И тут
                 .pressSubmit();
 
         registrationFormPage.checkTitleTableAndTableResult("Thanks for submitting the form")
-                .checkResults("Student Name", td.NAME + " " + td.LAST_NAME)
-                .checkResults("Student Email", td.EMAIL)
-                .checkResults("Gender", td.GENDER)
-                .checkResults("Mobile", td.PHONE)
-                .checkResults("Date of Birth", td.DAY + " " + td.MONTH + "," + td.YEAR)
-                .checkResults("Subjects", td.SUBJECT)
-                .checkResults("Hobbies", td.HOBBIES)
-                .checkResults("Picture", td.FILE)
-                .checkResults("Address", td.ADDRESS)
-                .checkResults("State and City", td.STATE + " " + td.CITY);
+                .checkResults("Student Name", u.getName() + " " + u.getLastName())
+                .checkResults("Student Email", u.getEmail())
+                .checkResults("Gender", u.getGender())
+                .checkResults("Mobile", u.getPhone())
+                .checkResults("Date of Birth", u.getDay() + " " + u.getMonth() + "," + u.getYear())
+                .checkResults("Subjects", u.getSubjects())
+                .checkResults("Hobbies", u.getHobbies())
+                .checkResults("Picture", u.getFiles())
+                .checkResults("Address", u.getAddress())
+                .checkResults("State and City", "NCR" + " " + "Delhi");
     }
 }
